9c535abaecb40a503bf801308ee94181a9e135c1
madeeha-anjum <maadeeeehaa@gmail.com>
2022-07-05 13:52:57 -0600

    layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,28 @@
-import React, { useState } from 'react';
+import Footer from './components/Footer';
+import Input from './components/Input';
 
 function App() {
   return (
-    <div>
-      <header className='my-auto shadow-2xl bg-accent-fuchsia-200 shadow-slate-400'>
-        <h1 className='text-2xl text-center text-slate-900'>Hello World</h1>
-      </header>
-    </div>
+    <main className='flex flex-col items-center min-h-screen bg-gradient-to-bl to-indigo from-steel-blue'>
+      <section className='w-full pt-8'>
+        <div className='px-2 sm:px-4 max-w-7xl'>
+          <h1 className='text-3xl font-semibold break-words'>Pushto.site</h1>
+        </div>
+      </section>
+
+      <section className='flex flex-col flex-1 w-full'>
+        <section className='px-2 my-auto sm:px-4'>
+          <div className='mx-auto max-w-7xl'>
+            <Input />
+          </div>
+        </section>
+        <section className='px-2 sm:px-4'>
+          <div className='pt-20 mx-auto text-xl max-w-7xl sm:text-2xl '>
+            <Footer />
+          </div>
+        </section>
+      </section>
+    </main>
   );
 }
 

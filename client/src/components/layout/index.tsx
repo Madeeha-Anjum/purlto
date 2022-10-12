49944b36e9c81641288ff07331a4dee37deef601
madeeha-anjum <maadeeeehaa@gmail.com>
2022-10-12 16:33:00 -0600

    added SUC

--- client/src/components/layout/index.tsx
+++ client/src/components/layout/index.tsx
@@ -7,27 +7,34 @@ type Props = {
 
 function Layout({ children }: Props) {
   return (
-    <main className='bg-gradient-to-bl via-indigo-blue to-steel-blue from-indigo-purple'>
-      <div className='relative flex flex-col items-center min-h-screen overflow-clip'>
-        <Header />
-
-        <section className='flex flex-col flex-1 w-full'>
-          <div className='px-2 my-auto sm:px-4'>
-            {/* center the main content  */}
-            {children}
-          </div>
-        </section>
-
-        <Footer />
-
-        {/* Bubbles! */}
-        <span className='absolute scale-150 bg-transparent border rounded-full w-60 h-60 top-10 -right-10 border-cool-grey/10'></span>
-        <span className='absolute w-2.5 h-2.5 rounded-full bg-cool-grey/10 right-44 top-48'></span>
-        <span className='absolute w-2.5 h-2.5 rounded-full bg-cool-grey/10 left-44 top-72'></span>
-        <span className='absolute w-2.5 h-2.5 rounded-full bg-cool-grey/10 left-60 bottom-44'></span>
-        <span className='absolute w-2.5 h-2.5 rounded-full bg-cool-grey/10 right-60 bottom-32'></span>
+    <>
+      <div className='z-1 absolute w-full bg-green-500'>
+        <h1 className='text-xl text-center text-black'>
+          Site Under Construction
+        </h1>
       </div>
-    </main>
+      <main className='bg-gradient-to-bl via-indigo-blue to-steel-blue from-indigo-purple'>
+        <div className='relative flex flex-col items-center min-h-screen overflow-clip'>
+          <Header />
+
+          <section className='flex flex-col flex-1 w-full'>
+            <div className='px-2 my-auto sm:px-4'>
+              {/* center the main content  */}
+              {children}
+            </div>
+          </section>
+
+          <Footer />
+
+          {/* Bubbles! */}
+          <span className='absolute scale-150 bg-transparent border rounded-full w-60 h-60 top-10 -right-10 border-cool-grey/10'></span>
+          <span className='absolute w-2.5 h-2.5 rounded-full bg-cool-grey/10 right-44 top-48'></span>
+          <span className='absolute w-2.5 h-2.5 rounded-full bg-cool-grey/10 left-44 top-72'></span>
+          <span className='absolute w-2.5 h-2.5 rounded-full bg-cool-grey/10 left-60 bottom-44'></span>
+          <span className='absolute w-2.5 h-2.5 rounded-full bg-cool-grey/10 right-60 bottom-32'></span>
+        </div>
+      </main>
+    </>
   );
 }
 
